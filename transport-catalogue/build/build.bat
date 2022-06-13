13e2cd5765986baa8b269cca0f4f7e06951a55e6
Vladislav Berestenko <valdislaf@gmail.com>
2022-06-13 14:25:38 +0300

    Sprint 14 Iteration 1_2

--- transport-catalogue/build/build.bat
+++
@@ -1,3 +0,0 @@
-cmake D:\VLAD\C++\practicum.yandex\lessons_g++\Sprint_14 -DCMAKE_PREFIX_PATH=/path/to/protobuf/package
-cmake --build . --config Debug 
-pause
--- transport-catalogue/transport_catalogue.proto.bat
+++
@@ -1,2 +0,0 @@
-D:\VLAD\C++\protobuf-3.21.1\path\to\protobuf\package\Debug\protoc.exe --cpp_out . transport_catalogue.proto
-pause
